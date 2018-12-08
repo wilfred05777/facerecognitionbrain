8b331df7443d5705c7bec25d36523b92e5c5c8fe
Wilfred Erdo <wilfred05777@gmail.com>
2018-12-08 10:49:59 +0800

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
 import './App.css';
 
 // import logo from './logo.svg';
 // import Navigation from './../components/Navigation';
 
+const particlesOptions ={
+  particles: {
+    number:{
+      value: 30,
+      density:{
+        enable: true,
+        value_are: 800
+      }
+    }
+  }    
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' 
+          params={particlesOptions} 
+        />
         <Navigation />
         <Logo />
         <Rank />
